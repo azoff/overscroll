7f7c12661b327ea46fc8d04674c4a6360f6239b2
Johannes Schickling <schickling.j@gmail.com>
2013-05-26 10:20:09 +0200

    Merge pull request #99

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			files: [
+				'Gruntfile.js',
+				'src/jquery.overscroll.js',
+				'test/*.js',
+				'test/modules/*.js',
+				'test/drift-canvas/js/app.js'
+			],
+			options: {
+				// options here to override JSHint defaults
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					base: './'
+				}
+			},
+			driftcanvas: {
+				options: {
+					port: 8999,
+					base: './test/',
+					keepalive: true
+				}
+			}
+		},
+
+		casperjs: {
+			files: ['test/*.js']
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-casperjs');
+
+
+	grunt.registerTask('test', [
+		'jshint',
+		'connect:server',
+		'casperjs'
+	]);
+
+	grunt.registerTask('default', [
+		'test'
+	]);
+
+};
